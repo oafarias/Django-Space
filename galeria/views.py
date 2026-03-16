71d2ef9b3242dd0c51eb06ea7c06e0738d3c7156
Alef Farias <alef.alves1800@gmail.com>
2026-03-16 17:28:46 -0300

    Incluido BD

--- galeria/views.py
+++ galeria/views.py
@@ -1,14 +1,10 @@
 from django.shortcuts import render
+from galeria.models import Fotografia
 
 def index(request):
-    dados = {
-    1: {"nome": "Nebulosa de Carina",
-        "legenda": "webbtelescope.org / NASA / James Webb"},
-    2: {"nome": "Galáxia NGC 1079",
-        "legenda": "nasa.org / NASA /Hubble"}
-    }
+    fotografias = Fotografia.objects.all()
     
-    return render(request, 'galeria/index.html', {"cards": dados})
+    return render(request, 'galeria/index.html', {"cards": fotografias})
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
