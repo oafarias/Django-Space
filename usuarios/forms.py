7cb97b733a84b8af0bbf0e52651c7963b5eb05d4
Alef Alves Farias <alef.alves1800@gmail.com>
2026-03-20 21:21:31 -0300

    feat: formularios de cadastro e login

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -23,3 +23,52 @@ class LoginForms(forms.Form):
             }
         )
     )
+
+class CadastroForms(forms.Form):
+    nome_cadastro=forms.CharField(
+        label="Nome de Cadastro",
+        required=True,
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Ex.: João Silva"
+            }
+        )
+    )
+
+    email=forms.EmailField(
+         label="Email",
+        required=True,
+        max_length=100,
+        widget=forms.EmailInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "joaosilva@gmail.com"
+            }
+        )
+    )
+
+    senha_1=forms.CharField(
+        label="Senha",
+        required=True,
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Digite Sua Senha"
+            }
+        )
+    )
+
+    senha_2=forms.CharField(
+        label="Confirme sua Senha",
+        required=True,
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Digite Sua Senha Novamente"
+            }
+        )
+    )
